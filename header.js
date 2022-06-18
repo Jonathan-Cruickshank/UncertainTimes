e3f9d14b44420fa70998452f977b3f1e04e30d1e
APrinceofCats <33228678+APrinceofCats@users.noreply.github.com>
2022-06-18 16:50:09 +0100

    Update header.js

--- header.js
+++ header.js
@@ -92,7 +92,7 @@ li.sources{
       <ul class="nav-bar">
     <li><a href="index.html">HOME</a> |</li>
     <li><a href="CV.html">ABOUT (CV)</a> |</li>
-    <li><a href="Article\Tags.html">TOPICS </a>|</li>
+    <li><a href="/Article/Tags.html">TOPICS </a>|</li>
 
     <li class="dropdown">
       <a href="javascript:void(0)" class="dropbtn">SOCIALS</a>
