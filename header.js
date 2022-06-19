da2f231b9b23fb75b4ffe9f235d507e5f811b004
APrinceofCats <33228678+APrinceofCats@users.noreply.github.com>
2022-06-19 02:57:25 +0100

    Update header.js

--- header.js
+++ header.js
@@ -19,47 +19,8 @@ a:active {
 }
 
 /*NAV BAR/DROPDOWN*/
-ul.nav-bar > li {
-  text-align: center;
-  display: inline-block;
-}
-
-  li a, .dropbtn {
-  display: inline-block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li.dropdown {
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  padding: 12px 16px;
-  border: 1px dashed black;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {
-  background-color: #f1f1f1;
-  display: block;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
+ul.nav-bar > li{
+display: inline-block;
 }
 </style>
 
@@ -79,17 +40,6 @@ li.dropdown {
     <li><a href="/../CV.html">ABOUT (CV)</a> |</li>
     <li><a href="/../Article/Tags/Tags.html">TOPICS </a>|</li>
 
-    <li class="dropdown">
-      <a href="javascript:void(0)" class="dropbtn">SOCIALS</a>
-      <div class="dropdown-content">
-        <a href="https://gab.com/A_Prince_of_Cats">Gab</a>
-        <a href="https://twitter.com/JCruin">Twitter</a>
-        <a href="https://www.youtube.com/channel/UCcAbMxs9U3KW7WxkNhSg_8Q">Youtube</a>
-        <a href="https://www.patreon.com/user?u=19009526">Patreon</a>
-        <a href="https://www.ko-fi.com/I2I85CW5">Ko-fi</a>
-        <a href="https://github.com/APrinceofCats">GitHub</a>
-      </div>
-    </li>
     </ul>
 </div>
 <hr/>
