e206e9bef1739eb975b5da6309f04c81379e4ac2
APrinceofCats <33228678+APrinceofCats@users.noreply.github.com>
2024-02-19 19:01:14 +0000

    Update header.js
    Signed-off-by: APrinceofCats <33228678+APrinceofCats@users.noreply.github.com>

--- header.js
+++ header.js
@@ -67,8 +67,8 @@ li.dropdown {
 <div align = "center">
 
       <ul class="nav-bar">
-    <li><a href="/index.html">HOME</a> |</li>
-    <li><a href="/CV.html">ABOUT (CV)</a> |</li>
+    <li><a href="../index.html">HOME</a> |</li>
+    <li><a href="../CV.html">ABOUT (CV)</a> |</li>
     <li><a href="/../Article/Tags/Topics.html">TOPICS</a> |</li>
 
     <li class="dropdown">
