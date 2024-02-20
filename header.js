038f4bdfc2a4b52901e9a8c9e14f9b3fe27cecd5
APrinceofCats <33228678+APrinceofCats@users.noreply.github.com>
2024-02-20 21:37:41 +0000

    Update header.js
    Signed-off-by: APrinceofCats <33228678+APrinceofCats@users.noreply.github.com>

--- header.js
+++ header.js
@@ -74,11 +74,7 @@ li.dropdown {
     <li class="dropdown">
       <a href="javascript:void(0)" class="dropbtn">SOCIALS</a>
       <div class="dropdown-content">
-        <a href="https://gab.com/A_Prince_of_Cats">Gab</a>
-        <a href="https://twitter.com/JCruin">Twitter</a>
         <a href="https://www.youtube.com/channel/UCcAbMxs9U3KW7WxkNhSg_8Q">Youtube</a>
-        <a href="https://www.patreon.com/user?u=19009526">Patreon</a>
-        <a href="https://www.ko-fi.com/I2I85CW5">Ko-fi</a>
         <a href="https://github.com/APrinceofCats">GitHub</a>
     </li>
 
